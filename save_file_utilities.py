ae24e3ae452fe88c1dc162383e30f58db77e6619
Domenico M. Bianco <domenicom.bianco@gmail.com>
2018-05-03 00:07:14 +0200

    new class

--- save_file_utilities.py
+++ save_file_utilities.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import datetime
+import re
 
 
 class FileManager():
@@ -39,17 +40,63 @@ class FileManager():
             try:
                 if os.path.isfile(file_path):
                     os.remove(file_path)
-            except Exception, e:
+            except Exception as e:
                 err_msg = "Cannot remove " + file_path + ". MESSAGE: " + str(e)
                 error_occurred = True
                 if logging:
                     logging.error(err_msg)
                 else:
-                    print err_msg
+                    print(err_msg)
         if error_occurred:
             return False
         else:
             return True
+
+
+
+class FilenamesWithId():
+    '''
+    The filename format is
+    base_filename**sep**key.ext
+    '''
+
+    sep = "__"
+
+    def __init__(self, dir, base_filename, extension):
+        if not os.path.isdir(dir):
+            raise AssertionError # <--- CHECK THIS!!!
+        self.base_dir = dir
+        self.base_filename = base_filename
+        self.extension = extension
+        self._get_keys()
+        
+
+    def list_files(self, only_id_files=True):
+        filenames = os.listdir(self.base_dir)
+        if only_id_files:
+            filenames = [f for f in filenames if self.sep in f]
+        return filenames
+
+
+    def get_already_generated_keys(self):
+        self._get_keys()
+        return list(self.old_keys)
+
+
+    def _get_keys(self):
+        filenames = [f for f in os.listdir(self.base_dir) if self.sep in f]
+        keys = map(lambda x: x.split(self.sep)[1], filenames)
+        keys = list(map(lambda x: x.replace("." + self.extension, ""), keys))
+        self.old_keys = set(keys)
+        assert len(self.old_keys) == len(keys)
+
+
+    def key_exists(self, key):
+        self._get_keys()
+        if key in self.old_keys:
+            return True
+        else:
+            return False
 
 
 
@@ -63,7 +110,7 @@ def get_completed_files(completed_file_path):
     return completed_files
 
 
-def get_all_days_between(d1, d2, date_format=DATE_FORMAT):
+def get_all_days_between(d1, d2, date_format="%Y-%m-%d %H:%M:%s"):
     '''
     It returns all the days between d1 and d2.
     date_format is the format of the aforementioned dates.
@@ -125,14 +172,6 @@ def get_files_to_process(input_dir, output_dir=None, completed_file_path=None,
     return sorted(list(res_files))
 
 
-def get_filenames_dates(start_date, end_date, input_format=DATE_FORMAT, output_format=FILE_DATE_FORMAT):
-    dates = get_all_days_between(start_date, end_date, date_format=input_format)
-    dates = map(lambda x: datetime.datetime.strftime(x, format=output_format),
-                            dates)
-    dates = set(dates)
-    return dates
-
-
 def get_all_dates(dates_dict, dir=None, return_one_list=False):
     train_start_date = dates_dict["train_start_date"]
     train_end_date = dates_dict["train_end_date"]
@@ -181,8 +220,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Test the functionalities of the class')
 
-    function_list = ("id_from_datetime", "id_from_timestamp", "n_characters_ids",
-                     "generate_random_id", "hash_object_to_id")
+    function_list = ("FilenamesWithId", )
     for x in function_list:
         cmd = '--{}'.format(x)
         help = 'Test {} function.'.format(x)
@@ -195,10 +233,28 @@ if __name__ == "__main__":
         for x in args:
             args[x] = True
 
-    if args["id_from_datetime"]:
-        print("TESTING id_from_datetime")
-        print("First id: {}".format(id_from_datetime()))
-        time.sleep(2)
-        print("Sleeping for 2 seconds.")
-        print("Second id: {}".format(id_from_datetime()))
-        print()
+    if args["FilenamesWithId"]:
+        print("TESTING FilenamesWithId")
+        test_dir = "./TEST/"
+        base_filename = "test"
+        keys = list(map(lambda x: str(x), range(10)))
+        extension = "txt"
+        sep = "__"
+        print("Creating a directory {}".format(test_dir))
+        print("Base filename: {}".format(base_filename))
+        print("Extension: {}".format(extension))
+        print("Keys: {}".format(keys))
+        if not os.path.isdir(test_dir):
+            os.mkdir(test_dir)
+            for k in keys:
+                filename = base_filename + sep + k + "." + extension
+                open(test_dir + filename, "w+")
+        file_manager = FilenamesWithId(test_dir, base_filename, extension)
+        new_keys = list(map(lambda x: str(x), range(5,15)))
+        print("New keys: {}".format(new_keys))
+        old_keys = file_manager.get_already_generated_keys()
+        print("Old keys: {}".format(old_keys))
+        for k in new_keys:
+            exist = file_manager.key_exists(k)
+            exist = "" if exist else "DOES NOT"
+            print("Key {} {} exist".format(k, exist))
